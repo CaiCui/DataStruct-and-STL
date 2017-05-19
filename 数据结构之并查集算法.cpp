c7bac2d746d63f54a3807acfd363eeef971067b3
崔崔崔 <buaacyf@126.com>
2017-05-19 22:06:03 +0800

    Update 数据结构之并查集算法.cpp

--- 数据结构之并查集算法.cpp
+++ 数据结构之并查集算法.cpp
@@ -2,38 +2,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 /*
-  ʡѡIO���鼯:nion-set ��Ҫ�����ж϶��Ԫ���Ƿ�����ͬһ�����ϣ���ͼ����ͨ��֧�����������ж�ͼ�еĻ��Ĵ���
-  find()���ܣ����Ҹ��ڵ�ͬʱ�������ṹ����һ����ѹ����һ�������������ͬ�����γ�ɭ��
+  省选IO并查集:union-set 主要用于判断多个元素是否属于同一个集合，如图的连通分支，或者用于判定图中的环的存在
+  find()功能：查找父节点同时更新树结构，由一条链压缩成一棵两层的树，不同的树形成森林
         A    ...      B
      / / \ \       / / \ \
      1 2 3..n      d c s ..3
-  ���Ӷȣ���ʵ�ϣ�·��ѹ����Ĳ��鼯�ĸ��Ӷ���һ����С�ĳ�����
+  复杂度：事实上，路经压缩后的并查集的复杂度是一个很小的常数。
 */
 #define MAXN 0x0fffffff
-int F[MAXN];//���鼯����
-int Find(int x)//�ڵ���Һͽṹ���£��ݹ�д��������RE
+int F[MAXN];//并查集数组
+int Find(int x)//节点查找和结构更新，递归写法，容易RE
 {
-    //����һ��if(F[x]==-1)return x;
+    //方法一、if(F[x]==-1)return x;
     if(F[x]==x)return x;
     return F[x]=Find(F[x]);
 }
-int Find2(int x)//�ڵ���Һͽṹ���£��ǵݹ�д����������RE
+int Find2(int x)//节点查找和结构更新，非递归写法，不容易RE
 {
     int root=x;
-    while(root!=F[root])//�ҵ����ڵ�r
+    while(root!=F[root])//找到根节点r
     {
         root=F[root];
     }
     int y=x;
-    while(y!=root)//·��ѹ������Ϊ2�����ṹ
+    while(y!=root)//路径压缩，变为2层树结构
     {
       int tmp=F[y];
-      F[y]=root;//�ӽڵ㸸�׸���Ϊ���ڵ�r
-      y=tmp;//�ظ��Ҹ��ڵ�
+      F[y]=root;//子节点父亲更新为根节点r
+      y=tmp;//重复找父节点
     }
     return root;
 }
-void join(int x, int y)//����Ԫ��
+void join(int x, int y)//加入元素
 {
     int t1,t2;
     t1=Find(x);
@@ -41,9 +41,9 @@ void join(int x, int y)//����Ԫ��
     if(t1!=t2)
         F[t1]=t2;
 }
-void init(int n)//��ʼ�����鼯
+void init(int n)//初始化并查集
 {
-    //����һ��memset(F,-1,sizeof(F))
+    //方法一、memset(F,-1,sizeof(F))
     for(int i=1;i<=n;i++)
     {
         F[i]=i;
